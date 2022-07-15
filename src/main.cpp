5adabec045b2ea64fd58270b903415ca70bf4567
Ryan Hartlage <ryanplusplus@gmail.com>
2022-07-15 07:37:58 -0400

    Removed ring buffer test code

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,6 @@
 
 #include "board.hpp"
 #include "tiny/Timer.hpp"
-#include "tiny/RingBuffer.hpp"
 
 using namespace tiny;
 
@@ -18,15 +17,11 @@ int main()
   Watchdog watchdog{timer_group};
   Heartbeat heartbeat{timer_group};
 
-  RingBuffer<int, 5> ring_buffer;
-  ring_buffer.insert(4);
-
   Interrupts::enable();
 
   while(1) {
     if(timer_group.run()) {
       Interrupts::wait_for_interrupt();
     }
-    ring_buffer.remove();
   }
 }
